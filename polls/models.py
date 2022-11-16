ccbd3c2a56349d35cddc3279300f7dbc049d7ddd
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-11-16 11:50:24 +0100

    Add migrations for products, customers, orders

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,31 @@
-import datetime
+from datetime import datetime
 
 from django.db import models
 from django.utils import timezone
+
+
+class Products(models.Model):
+    # id = models.AutoField(primary_key=True)
+    product = models.CharField(max_length=100, blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=100)
+    description = models.TextField(blank=True, null=True)
+
+
+class Customer(models.Model):
+    # id = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=100, blank=False)
+    lastName = models.CharField(max_length=100, blank=False)
+    street = models.CharField(max_length=100, blank=False)
+    plz = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(blank=False)
+
+
+class Orders(models.Model):
+    # id = models.AutoField(primary_key=True)
+    orderDate = models.DateTimeField(default=datetime.now)
+    articleOrdered = models.ForeignKey(Products, on_delete=models.CASCADE)
+    customerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    amount = models.IntegerField()
 
 
 class Question(models.Model):
