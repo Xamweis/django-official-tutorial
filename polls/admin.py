0730e48589a982c0d5f202486d3f0d8f4f23d73a
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-11-17 10:02:44 +0100

    Add django-import-export library

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from import_export import resources
 
 from .models import Product, Customer, Order
 
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
+
+
+class ProductResource(resources.ModelResource):
+    class Meta:
+        model = Product
